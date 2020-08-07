dd53665f8356868f3589f709e19ac7d99c0991cf
MobChar <65935190+MobChar@users.noreply.github.com>
2020-08-07 23:12:14 +0700

    Update rabbitSetup.js

--- rabbitSetup.js
+++ rabbitSetup.js
@@ -16,7 +16,7 @@ async function setUpRabbit() {
 
             rabbitChannel.consume('image-server-new-image', function (payload) {
                 let data = JSON.parse(payload.content);
-                fs.writeFile('./uploadImage/' + data.fileName, Buffer.from(data.fileBuffer),{ flag: 'w' }, (err) => {
+                fs.writeFile(__dirname+'/uploadImage/' + data.fileName, Buffer.from(data.fileBuffer),{ flag: 'w' }, (err) => {
                     if (err) console.log(err);
                 });
                 console.log(__dirname+'/uploadImage/' + data.fileName);
